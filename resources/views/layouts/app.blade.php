88d19c6e7ae0d655ded1d5a2fc06e24661b07fe8
Everth Paredes <epe0413@gmail.com>
2022-07-23 20:28:00 -0600

    Creamos el formulario de registro de usuario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,25 +8,25 @@
         @vite('resources/css/app.css')
     </head>
     <body class="bg-gray-100">
-        <header class="p-5 border-b bg-white shadow">
+        <header class="p-3 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
-                <h1 class="text-3xl font-black">
+                <h1 class="text-lg font-black">
                     DevStagram
                 </h1>
 
                 <nav class="flex gap-2 items-center">
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuenta</a>
+                    <a class="font-bold uppercase text-gray-600 text-xs" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-xs" href="/crear-cuenta">Crear Cuenta</a>
                 </nav>
             </div>   
         </header>
-        <main class="container mx-auto mt-10">
-            <h2 class="font-black text-center text-3xl mb-10">
+        <main class="container mx-auto mt-5">
+            <h2 class="font-black text-center text-2xl mb-5">
                 @yield('titulo')
             </h2>
             @yield('contenido')
         </main>
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="mt-5 text-center p-3 text-gray-500 font-bold uppercase">
             DevStagram - Todos los derechos reservados 
             {{ now()->year }}
         </footer>
