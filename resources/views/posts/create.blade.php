43878b34bcfa80233ed152ce7be154c589525ac1
Everth Paredes <epe0413@gmail.com>
2022-08-16 16:34:52 -0600

    Leer las imágenes del Request

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,7 +13,7 @@
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-5">
             <form action="{{route('imagenes.store')}}" method="POST" enctype="multipart/" id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
-                
+                @csrf
             </form>
         </div>
         <div class="md:w-1/2 p-5 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
