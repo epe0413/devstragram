9b35a3813c1f48ea2034c7e3a882c8ad41f5daaf
Everth Paredes <epe0413@gmail.com>
2022-08-16 14:58:12 -0600

    Instalando Dropzone

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,7 +8,9 @@
 
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-5">
-            imagen aquí
+            <form action="/IMAGENES" id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+                
+            </form>
         </div>
         <div class="md:w-1/2 p-5 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
             <form action="{{ route('register') }}" method="POST" novalidate>
