5f1b6b536a63b9a805fbb67c155aac0b164195fc
Everth Paredes <epe0413@gmail.com>
2022-08-18 15:14:27 -0600

    Paginando las publicaciones

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -45,6 +45,10 @@
                     </div>
                 @endforeach
             </div>
+
+            <div>
+                {{ $posts->links('pagination::tailwind')}}
+            </div>
         
         @else
             <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
