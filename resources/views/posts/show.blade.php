c28e419151e8b11cd86627bd8b89c2071e7f51e0
Everth Paredes <epe0413@gmail.com>
2022-08-19 15:08:13 -0600

    Añadiendo una caja de comentarios

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,8 +23,36 @@
                 <p class="mt-3">{{ $post->descripcion}}</p>
             </div>
         </div>
-        <div class="md:w-1/2">
-            2
+        <div class="md:w-1/2 px-3">
+            <div class="shadow bg-white p-3 mb-3 rounded-lg">
+                <p class="text-xl font-bold text-center mb-4">Agrega un Nuevo Comentario</p>
+
+                <form>
+                    <div class="mb-2">
+                        <label for="Comentario" class="mb-1 text-sm block text-gray-500 font-bold">
+                            Añade un comentario
+                        </label>
+                        <textarea
+                            id="Comentario"
+                            name="Comentario"
+                            placeholder="Agrega un comentario"
+                            class="border p-2 text-sm w-full rounded-lg @error('name')
+                            border-red-500 @enderror"
+                        ></textarea>
+                        @error('comentario')
+                            <p class="bg-red-500 text-white my-2 text-sm p-2 rounded-lg text-center">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+    
+                    <input
+                        type="submit"
+                        value="Comentar"
+                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer font-bold w-full p-2 text-sm text-white rounded-lg"    
+                    >
+                </form>
+            </div>
         </div>
     </div>
 @endsection
