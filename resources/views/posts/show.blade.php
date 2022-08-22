7f396fc79a4f46ddb56e21f0891ce9b2e43b95d3
Everth Paredes <epe0413@gmail.com>
2022-08-21 19:49:13 -0600

    Creando el botón de Like

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,7 +12,17 @@
                 alt="Imagen del post {{ $post->titulo }}"
                 class="rounded-xl"
             >
-            <div class="p-3">
+            <div class="p-3 flex items-center gap-2">
+                <form>
+                    <div class="my-1">
+                        <button type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                            </svg>
+                        </button>
+                    </div>
+                </form>
+
                 <p>0 Likes</p>
             </div>
             <div>
