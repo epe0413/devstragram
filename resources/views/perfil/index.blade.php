b2a2963986ef94aea9ecb7cfe3a25c5f280b766b
Everth Paredes <epe0413@gmail.com>
2022-08-22 21:00:28 -0600

    Creando el formulario para editar perfil

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -5,5 +5,47 @@
 @endsection
 
 @section('contenido')
-    
+    <div class="md:flex md:justify-center">
+        <div class="md:w-1/2 bg-white shadow p-3">
+            <form class="mt-5 md:mt-0">
+                <div class="mb-2">
+                    <label for="username" class="mb-1 text-sm block text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu Nombre de Usuario"
+                        class="border p-2 text-sm w-full rounded-lg @error('username') border-red-500 @enderror"
+                        value="{{ auth()->user()->username }}"
+                    />
+                    @error('username')
+                        <p class="bg-red-500 text-white my-2 text-sm p-2 rounded-lg text-center">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <div class="mb-2">
+                    <label for="username" class="mb-1 text-sm block text-gray-500 font-bold">
+                        Imagen Perfil
+                    </label>
+                    <input
+                        id="imagen"
+                        name="imagen"
+                        type="file"
+                        class="border p-2 text-sm w-full rounded-lg"
+                        value=""
+                        accept=".jpg, .jpeg, .png"
+                    />
+                </div>
+
+                <input
+                    type="submit"
+                    value="Guardar Cambios"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer font-bold w-full p-2 text-sm text-white rounded-lg"    
+                >
+            </form>
+        </div>
+    </div>
 @endsection
