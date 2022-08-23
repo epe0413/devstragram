2f6f61a548fe4ae1efc655851f62f7b753842650
Everth Paredes <epe0413@gmail.com>
2022-08-22 20:21:27 -0600

    Proteger la edición de Perfiles

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PerfilController;
 use PhpParser\Node\Expr\BinaryOp\LogicalOr;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ComentarioController;
-use App\Http\Controllers\LikeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,3 +46,7 @@ Route::post('/imagenes',[ImagenController::class, 'store'])->name('imagenes.stor
 // Like a las fotos
 Route::post('/post/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
 Route::delete('/post/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+// Rutas para el perfil
+Route::get('{user:username}/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
+Route::post('{user:username}/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
