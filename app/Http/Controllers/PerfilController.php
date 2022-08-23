ecb9c16f36754f5e936bfddbeb75c1637bbb1ede
Everth Paredes <epe0413@gmail.com>
2022-08-23 10:54:08 -0600

    Creando la migración para añadir imagenes al perfil del usuario

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -36,14 +36,14 @@ class PerfilController extends Controller
             $imagenServidor = Image::make($imagen);
             $imagenServidor->fit(1000, 1000);
 
-            $imagenPath = public_path('uploads') . '/'  . $nombreImagen;
+            $imagenPath = public_path('perfiles') . '/'  . $nombreImagen;
             $imagenServidor->save($imagenPath);
         }
         
         // Guardar cambios
         $usuario = User::find(auth()->user()->id);
         $usuario->username = $request->username;
-        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? null;
         $usuario->save();
 
         // Redireccionar usuario
