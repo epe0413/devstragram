524a469d4266cea383e2967217875505d79f3db1
Everth Paredes <epe0413@gmail.com>
2022-08-23 21:17:25 -0600

    Creando la página Principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,9 @@
     <body class="bg-gray-100">
         <header class="p-3 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
-                <h1 class="text-lg font-black">
+                <a href="{{ route('home') }}" class="text-lg font-black">
                     DevStagram
-                </h1>
+                </a>
 
                 @auth
                     <nav class="flex gap-2 items-center">
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use PhpParser\Node\Expr\BinaryOp\LogicalOr;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ComentarioController;
 use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,9 +24,7 @@ use App\Http\Controllers\FollowerController;
 |
 */
 
-Route::get('/', function () {
-    return view('principal');
-});
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
