b01113677498b997a7b91f5583e90b11894d9f2d
Everth Paredes <epe0413@gmail.com>
2022-08-24 11:59:54 -0600

    Agregando el contenido del componente

--- resources/views/components/listar-post.blade.php
+++ resources/views/components/listar-post.blade.php
@@ -1,3 +1,23 @@
 <div>
-  <h1>Mostrando Posts</h1>
+  @if($posts->count())
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          @foreach ($posts as $post)
+              <div>
+                  <a href="{{ route('posts.show', ['post' => $post, 'user'=>$post->user ]) }}">
+                      <img
+                          src="{{ asset('uploads') . '/' . $post->imagen }}"
+                          alt="Imagen del Post {{$post->titulo}}"
+                          class="rounded-md"
+                      >
+                  </a>
+              </div>
+          @endforeach
+      </div>
+
+      <div class="my-5">
+          {{ $posts->links('pagination::tailwind')}}
+      </div>
+  @else
+      <p class="text-center">No hay post, sigue a alguien para poder ver sus posts</p>
+  @endif
 </div>
