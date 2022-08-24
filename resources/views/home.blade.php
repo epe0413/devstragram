5058f4ebe071b9a0477f49f3fcd16de96a1694cf
Everth Paredes <epe0413@gmail.com>
2022-08-23 21:40:21 -0600

    Mostrar los posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,26 @@
 @endsection
 
 @section('contenido')
-    Contenido de esta página
+    @if($posts->count())
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            @foreach ($posts as $post)
+                <div>
+                    <a href="{{ route('posts.show', ['post' => $post, 'user'=>$post->user ]) }}">
+                        <img
+                            src="{{ asset('uploads') . '/' . $post->imagen }}"
+                            alt="Imagen del Post {{$post->titulo}}"
+                            class="rounded-md"
+                        >
+                    </a>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="my-5">
+            {{ $posts->links('pagination::tailwind')}}
+        </div>
+    @else
+        <p class="text-center">No hay post, sigue a alguien para poder ver sus posts</p>
+    @endif
 
 @endsection
