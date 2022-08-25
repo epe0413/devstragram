1bf73862aab6b1a3b2146ee2a3ec59acf05578dd
Everth Paredes <epe0413@gmail.com>
2022-08-24 21:52:55 -0600

    Mover la lógica de Likes y dejar de dar like a Livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -10,7 +10,14 @@ class LikePost extends Component
 
     public function like()
     {
-        return "desde la función de Like";
+        if( $this->post->checkLike(auth()->user()))
+        {
+            $this->post->likes()->where('post_id', $this->post->id)->delete();
+        }else{
+            $this->post->likes()->create([
+                'user_id' => auth()->user()->id
+            ]);
+        }
     }
 
     public function render()
