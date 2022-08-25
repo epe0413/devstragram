095796ef06f8971696dce5882585a82fcd6a1369
Everth Paredes <epe0413@gmail.com>
2022-08-25 16:20:49 -0600

    Actualizar el Contador de Likes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,37 +16,9 @@
                 @auth
                     <livewire:like-post :post="$post"/>
                     
-                    {{-- @if($post->checkLike(auth()->user()))
-                        <form method="POST" action="{{ route('posts.likes.destroy', $post) }}">
-                            @method('DELETE')
-                            @csrf
-                            <div class="my-1">
-                                <button type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="red">
-                                        <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </form>
-                    @else
-                        <form method="POST" action="{{ route('posts.likes.store', $post) }}">
-                            @csrf
-                            <div class="my-1">
-                                <button type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </form>
-                    @endif --}}
-                    
                 @endauth
 
-                <p class="font-bold">
-                    {{ $post->likes->count() }}
-                    <span class="font-normal">Likes</span>
-                </p>
+                
             </div>
             <div>
                 <p class="font-bold">{{ $post->user->username }}</p>
